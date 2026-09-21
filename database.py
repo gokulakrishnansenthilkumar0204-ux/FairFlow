74966efe6ceef22df19c155cdc95ce3214abe225
Gokulakrishnan Senthilkumar <gokulakrishnansenthilkumar0204@gmail.com>
2026-09-21 14:40:31 +0530

    Remove database URL assignment in database.py

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://test@localhost/fairflow_db"
+DATABASE_URL = 
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
